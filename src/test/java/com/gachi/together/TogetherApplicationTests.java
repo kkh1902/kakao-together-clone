da2efa44250998ce7ca2e80802d61212fba39955
kkh1902@naver.com <kkh1902@naver.com>
2023-06-25 23:21:04 +0900

    feat: project 생성

---
+++ src/test/java/com/gachi/together/TogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachi.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
